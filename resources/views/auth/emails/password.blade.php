4437889342507134eefe85afd258f19d69d65f4b
Esteban Ramos Fernandez <14365041+Estebangramosf@users.noreply.github.com>
2017-11-22 12:27:24 -0300

    update/add - add adding tildes in email password

--- resources/views/auth/emails/password.blade.php
+++ resources/views/auth/emails/password.blade.php
@@ -1,10 +1,10 @@
 Estimado/a:
-Junto con saludar se informa que se ha establecido una nueva forma de creacion de claves (password) para el ingreso del Sistema de Transmision Vertical VIH y SIFILIS.
+Junto con saludar se informa que se ha establecido una nueva forma de creaci&oacute;n de claves (password) para el ingreso del Sistema de Transmisi&oacute;n Vertical VIH y SIFILIS.
 
 <br><br>
-Para crear su clave debera hacer click en el enlace de a continacion, donde se le dirigirá a una nueva pantalla para que usted pueda crear su password e ingresar inmediatamente al sistema.
+Para crear su clave deber&aacute; hacer clic en el enlace de a continuaci&oacute;n, donde se le redireccionar&aacute; a una nueva pantalla para que usted pueda crear su password e ingresar inmediatamente al sistema.
 
 <br><br>
 
-Haga click aquí para crear su password:
+Haga click aqu&iacute; para crear su password:
 <a href="{{ $link = url('password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}"> {{ $link }} </a>
