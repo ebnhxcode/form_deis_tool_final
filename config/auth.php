8f9369c25bafedfab264aa51ee74239a72da8802
Esteban Ramos Fernandez <14365041+Estebangramosf@users.noreply.github.com>
2017-11-27 10:10:52 -0300

    update/add - update time expire for reset password link

--- config/auth.php
+++ config/auth.php
@@ -100,7 +100,7 @@ return [
             'provider' => 'users',
             'email' => 'auth.emails.password',
             'table' => 'password_resets',
-            'expire' => 2880,
+            'expire' => 10080,
         ],
     ],
 
