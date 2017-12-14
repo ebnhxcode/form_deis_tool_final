c0ef7831084254c7f3bc9cd8f1beb4dfba737f05
Esteban Ramos Fernandez <14365041+Estebangramosf@users.noreply.github.com>
2017-12-13 21:42:36 -0300

    update/add - adding fix in alert message.

--- resources/assets/js/app/api/controllers/FormularioController.js
+++ resources/assets/js/app/api/controllers/FormularioController.js
@@ -1399,7 +1399,7 @@ const FormularioController = new Vue({
                   }
                   swal({
                      title: "Advertencia",
-                     text: "Se informa que si el resultado 1 o 2 del examen VIH es Reactivo, se DEBE ingresar el dato de derivación, ya que no puede haber resultados reactivos sin derivación..",
+                     text: "Se informa que si el resultado 1 o 2 del examen VIH es Reactivo, se DEBE ingresar el dato de derivación a especialidades, ya que no puede haber resultados reactivos sin derivación..",
                      type: "warning",
                      confirmButtonClass: "btn-danger",
                      closeOnConfirm: false
@@ -1453,7 +1453,7 @@ const FormularioController = new Vue({
                   }
                   swal({
                      title: "Advertencia",
-                     text: "Se informa que si el resultado 1 o 2 del examen VIH es Reactivo, se DEBE ingresar el dato de derivación, ya que no puede haber resultados reactivos sin derivación..",
+                     text: "Se informa que si el resultado 1 o 2 del examen VIH es Reactivo, se DEBE ingresar el dato de derivación a especialidades, ya que no puede haber resultados reactivos sin derivación..",
                      type: "warning",
                      confirmButtonClass: "btn-danger",
                      closeOnConfirm: false
