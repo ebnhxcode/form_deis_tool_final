c6e12bfd4ef9a5dbbbf391b2692ee6e8aa170e06
Esteban Ramos Fernandez <14365041+Estebangramosf@users.noreply.github.com>
2017-12-20 20:35:39 -0300

    update/add - backup

--- resources/assets/js/app/api/controllers/FormularioController.js
+++ resources/assets/js/app/api/controllers/FormularioController.js
@@ -3092,7 +3092,7 @@ const FormularioController = new Vue({
                if (this.inputs[i].name == 'lugar_control_prenatal' ||
                   this.inputs[i].name == 'lugar_atencion_parto' ||
                   this.inputs[i].name == 'lugar_control_embarazo' ||
-                  this.inputs[i].name == 'establecimiento_control_sifilis' ||
+                  this.inputs[i].name == 'establecimiento_control_sifilis' +
                   this.inputs[i].name == 'establecimiento_control_vih' ||
                   this.inputs[i].name == 'atencion_parto'
                ) {
